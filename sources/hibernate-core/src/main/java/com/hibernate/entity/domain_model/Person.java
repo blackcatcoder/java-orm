2a1cd95c1cc937ecf2816e9d9dabeebb3971b65d
blackcat <vinv.2491@hotmail.com>
2024-03-25 21:16:40 +0700

    ud

---
+++ sources/hibernate-core/src/main/java/com/hibernate/entity/domain_model/Person.java
@@ -0,0 +1,18 @@
+package com.hibernate.entity.domain_model;
+
+import javax.persistence.MappedSuperclass;
+
+@MappedSuperclass
+public abstract class Person {
+	
+	private String owner;
+
+	public String getOwner() {
+		return owner;
+	}
+
+	public void setOwner(String owner) {
+		this.owner = owner;
+	}
+
+}
---
+++ sources/hibernate-core/src/test/java/com/hibernate/dao/test/domain_model/DomainModelTest.java
@@ -0,0 +1,41 @@
+package com.hibernate.dao.test.domain_model;
+
+import com.hibernate.dao.test.BaseDAOTest;
+import com.hibernate.entity.domain_model.Contact;
+import com.hibernate.entity.domain_model.Name;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.List;
+
+import org.junit.Test;
+
+public class DomainModelTest extends BaseDAOTest{
+	
+	@Test
+	public void testSaveData() {
+		Contact ct = new Contact();
+		ct.setNotes("anhvi");
+		ct.setOwner("owner");
+		
+		Name name = new Name();
+		name.setFirst("nong");
+		name.setLast("Vi");
+		name.setMiddle("van");
+		
+		ct.setName(name);
+		
+		em.persist(ct);
+		em.flush();
+		
+		List<Contact> rs = em.createQuery("select c from Contact c", Contact.class).getResultList();
+		
+		assertEquals(rs.size(), 1);
+		assertEquals(rs.get(0).getNotes(), "anhvi");
+		assertEquals(rs.get(0).getName().getFirst(), "nong");
+		
+		assertEquals(rs.get(0).getOwner(), "owner");
+		
+	}
+
+}
